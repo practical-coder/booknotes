9fba1dcd81e4ffaef958d9260f565def753c9a8d
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2021-10-10 21:15:20 +0200

    Notes: testing create/delete endpoints

--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -28,4 +30,39 @@ func TestList(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Greater(t, len(notes), 0)
+}
+
+func TestCreateDelete(t *testing.T) {
+	e := SetEngine()
+	os.Setenv("MONGODB_URI", "mongodb://localhost:27017")
+
+	note := book.Note{
+		Title: "Testing in Go",
+	}
+
+	jsonNote, _ := json.Marshal(note)
+	req, err := http.NewRequest("POST", "/notes", bytes.NewReader(jsonNote))
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+	e.ServeHTTP(w, req)
+
+	var respNote book.Note
+	json.Unmarshal([]byte(w.Body.String()), &respNote)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, note.Title, respNote.Title)
+
+	endpoint := fmt.Sprintf("/notes/%s", respNote.UUID)
+	req, err = http.NewRequest("DELETE", endpoint, nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w = httptest.NewRecorder()
+	e.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
 }
